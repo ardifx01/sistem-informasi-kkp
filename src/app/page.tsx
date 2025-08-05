c442720498199d4a8211a18aae6a0149cfc4cfe3
Glenn-Rhee <glennviktor5@gmail.com>
2025-08-05 13:46:02 +0700

    refactor: fixed home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,7 @@
 export default function HomePage() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const collectionRef = collection(db, "user");
-        const snapshot = await getDocs(collectionRef);
-        const result = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        console.log(result);
-      } catch (error) {
-        console.error("Gagal mengambil data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div>
-      <h1>Home Page</h1>
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>{JSON.stringify(item)}</li>
-        ))}
-      </ul>
+      <h1>Home page</h1>
     </div>
   );
 }
