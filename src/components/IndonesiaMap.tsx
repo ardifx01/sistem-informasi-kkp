f49fcc05fb715effb0463040eeee6942205d3415
davywibowo <davywibowo10@gmail.com>
2025-08-19 11:01:47 +0700

    fix:map indo

--- src/components/IndonesiaMap.tsx
+++ src/components/IndonesiaMap.tsx
@@ -2,16 +2,18 @@ import Image from "next/image";
 
 export default function IndonesiaMap() {
   return (
-    <div className="h-48 md:h-80 flex items-center justify-center relative overflow-hidden">
+    <div className="group h-48 md:h-80 lg:h-96 flex items-center justify-center ">
       <Image
         width={841}
         height={297}
-        src="/assets/indonesia-1.png"
-        alt="Peta Indonesia"
-        className="max-w-full max-h-full object-contain opacity-80 group-hover:opacity-100 transition-opacity duration-500 transform group-hover:scale-105"
+        src="/assets/puki.png"
+        alt="Peta Sebaran SDM Indonesia"
+        className="max-w-full max-h-full object-contain opacity-85 group-hover:opacity-100 transition-all duration-500 transform group-hover:scale-113"
+        priority
       />
 
-      <div className="absolute top-2 md:top-4 md:left-4 left-2 bg-white bg-opacity-90 rounded-lg p-1 text-xs font-semibold text-gray-700 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+      {/* Overlay Title */}
+      <div className="absolute top-2 right-2 bg-white bg-opacity-90 backdrop-blur-sm rounded p-1 text-xs font-medium text-gray-700 opacity-0 group-hover:opacity-100 transition-all duration-300 transform translate-y-2 group-hover:translate-y-0">
         🗺️ Peta Sebaran SDM Indonesia
       </div>
     </div>
